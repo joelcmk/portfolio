c2675e606dbcf13707f8d4bc2bea28f848e09046
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-08-14 13:46:58 -0500

    added image and change elements order

--- src/WorkCard.js
+++ src/WorkCard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Pokemon from './img/pokemon_1.png'
+import './WorkCard.css'
 
 class WorkCard extends Component {
   render() {
@@ -7,11 +9,17 @@ class WorkCard extends Component {
         <h1>Work</h1>
         <div className="grid">
           <div className="grid__item">
-            <h2><a className="a-work" href="pokemon.html">Pokemon jQuery</a></h2>
-            <span className="work-s">jQuery + Public API</span>
-
-            <button>Code</button>
-            <button>Aplication</button>
+            <div className="img-container">
+              <img className="img" src={Pokemon} />
+            </div>
+            <div>
+              <h2><a className="a-work" href="pokemon.html">Pokemon jQuery</a></h2>
+              <span className="work-s">jQuery + Public API</span>
+            </div>
+            <div>
+              <button>Code</button>
+              <button>Aplication</button>
+            </div>
           </div>
         </div>
       </section>
