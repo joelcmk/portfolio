d70aeffce56bb58882cc43f9b6e864bf1794f009
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-08-20 20:56:17 -0500

    Updates

---
+++ precache-manifest.19980ec01134adf938c69c0e2d1fd2e4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b295ee632e1e7e34d9d752bb8f5d0d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6317b6964b8680cbab37",
+    "url": "/portfolio/static/css/main.ead86ce8.chunk.css"
+  },
+  {
+    "revision": "698b56bef5202ad7d650",
+    "url": "/portfolio/static/js/2.e610272c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.e610272c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6317b6964b8680cbab37",
+    "url": "/portfolio/static/js/main.5d205ec1.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e9b42b4f3754f36db703f76e178d4848",
+    "url": "/portfolio/static/media/chat_app.e9b42b4f.png"
+  },
+  {
+    "revision": "109c31386ee02cc2f7b9f0cd3edaad58",
+    "url": "/portfolio/static/media/github.109c3138.svg"
+  },
+  {
+    "revision": "417516b83856df1d3cc32bee2c91534f",
+    "url": "/portfolio/static/media/joel_saucedo.417516b8.jpg"
+  },
+  {
+    "revision": "834b8104a4ed5e787e736e2feb651e5a",
+    "url": "/portfolio/static/media/linkedin.834b8104.svg"
+  },
+  {
+    "revision": "c2f3cc9ecb3b3d7c44ee9427a5690ebc",
+    "url": "/portfolio/static/media/list.c2f3cc9e.png"
+  },
+  {
+    "revision": "bf482be7e3c80aeef6ca5c8c5cede17d",
+    "url": "/portfolio/static/media/mail.bf482be7.svg"
+  },
+  {
+    "revision": "5041d856fd034aa2ce68a7b31c1b6a6a",
+    "url": "/portfolio/static/media/meetup.5041d856.png"
+  },
+  {
+    "revision": "c0ed8110f09ee731a64944c2f6ceea94",
+    "url": "/portfolio/static/media/myflix.c0ed8110.png"
+  },
+  {
+    "revision": "421fca465b9d4f3ed46f500077d254d6",
+    "url": "/portfolio/static/media/pokemon_1.421fca46.png"
+  },
+  {
+    "revision": "23df08a2d08c3d2dedfadca7732758f4",
+    "url": "/portfolio/static/media/texas.23df08a2.svg"
+  },
+  {
+    "revision": "3a5598c9cea5fee176705be8245bf3da",
+    "url": "/portfolio/static/media/twitter.3a5598c9.svg"
+  }
+]);
